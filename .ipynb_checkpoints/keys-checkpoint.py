e5f2975c68bbe0994c546e5bc9b7759357ed68a8
HssainAzaqy135 <kinghssain135@gmail.com>
2025-09-05 14:04:10 +0300

    Changed name of testing speechrecognition notebook

---
+++ .ipynb_checkpoints/keys-checkpoint.py
@@ -0,0 +1,5 @@
+OpenRouter: 
+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
+
+get_flac_converter in audio.py path:
+C:\Users\kingh\miniconda3\envs\voice_operator_env\Lib\site-packages\speech_recognition
